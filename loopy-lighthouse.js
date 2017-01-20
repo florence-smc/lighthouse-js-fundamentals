9e4721f70b8aa13313465110431ad7ab3388e184
Florence <smc.florence@gmail.com>
2017-01-20 06:34:04 +0000

    Added Loopy Lighthouse assignment

---
+++ loopy-lighthouse.js
@@ -0,0 +1,11 @@
+for (var i = 100; i <= 200; i++) {
+	console.log(i);
+}
+
+// if (num % 3 === 0) {
+//   console.log("Loopy");
+// } else if (num % 4 === 0) {
+//   console.log("Lighthouse");
+// } else if (num % 3 === 0 && num % 4 === 0) {
+//   console.log("LoopyLighthouse");
+// }
