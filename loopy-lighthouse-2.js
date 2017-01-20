28df39d723016ef334322e5676d530c5133cffb7
Florence <smc.florence@gmail.com>
2017-01-20 23:05:08 +0000

    Working on Loopy Lighthouse 2

---
+++ loopy-lighthouse-2.js
@@ -0,0 +1,24 @@
+function loopyLighthouse(range, multiples, words) {
+	var range = [];
+	var multiples = [];
+	var words = [" "];
+}
+
+// for (var i = 100; i <= 200; i++) {
+// 	if (i % 3 === 0 && i % 4 === 0) {
+// 		// filters the numbers that are multiples of 3 and 4 first
+// 		console.log("LoopyLighthouse");
+// 	} else if (i % 3 === 0) {
+// 		// filters numbers that are multiples of only 3
+// 		console.log("Loopy");
+// 	} else if (i % 4 === 0) {
+// 		// filters numbers that are multiples of 4 only
+// 		console.log("Lighthouse");
+// 	} else {
+// 		// and finally, logs all other numbers
+// 		console.log(i);
+// 	}
+// }
+  
+
+loopyLighthouse([100, 200], [2, 5], ["Batty", "Beacon"]);
