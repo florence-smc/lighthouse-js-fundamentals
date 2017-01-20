deabb92a90d3b66194d59143fae9b8c30899d8a3
Florence <smc.florence@gmail.com>
2017-01-20 23:27:26 +0000

    Moved for loop into the loopyLighthouse function and uncommented loop

--- loopy-lighthouse-2.js
+++ loopy-lighthouse-2.js
@@ -1,24 +1,27 @@
 function loopyLighthouse(range, multiples, words) {
+
 	var range = [];
 	var multiples = [];
-	var words = [" "];
-}
+	var words = [];
 
-// for (var i = 100; i <= 200; i++) {
-// 	if (i % 3 === 0 && i % 4 === 0) {
-// 		// filters the numbers that are multiples of 3 and 4 first
-// 		console.log("LoopyLighthouse");
-// 	} else if (i % 3 === 0) {
-// 		// filters numbers that are multiples of only 3
-// 		console.log("Loopy");
-// 	} else if (i % 4 === 0) {
-// 		// filters numbers that are multiples of 4 only
-// 		console.log("Lighthouse");
-// 	} else {
-// 		// and finally, logs all other numbers
-// 		console.log(i);
-// 	}
-// }
-  
+	for (var i = range[0]; i <= range[1]; i++) {
+
+		if (i % multiples[0] === 0 && i % multiples[1] === 0) {
+			// filters the numbers that are multiples of both numbers within the multiples array
+			console.log("LoopyLighthouse");
+		} else if (i % multiples[0] === 0) {
+			// filters numbers that are multiples of only the first number passed into the multiples parameter
+			// then console logs the first word passed into the words param
+			console.log(words[0]);
+		} else if (i % multiples[1] === 0) {
+			// filters numbers that are multiples of onlyl the second number passed into the multiples parameter
+			// then console logs the second word passed into the words param
+			console.log(words[1]);
+		} else {
+			// and finally, logs all other numbers
+			console.log(i);
+		}
+	}
+} 
 
 loopyLighthouse([100, 200], [2, 5], ["Batty", "Beacon"]);
