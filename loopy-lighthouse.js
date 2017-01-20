6eed0ee0aca122519010a7cbc0f86c3b8e3329ea
Florence <smc.florence@gmail.com>
2017-01-20 06:48:37 +0000

    re-arranged order of conditions, changed num to i, and added comments

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -1,11 +1,15 @@
 for (var i = 100; i <= 200; i++) {
-	if (num % 3 === 0) {
-	  console.log("Loopy");
-	} else if (num % 4 === 0) {
-	  console.log("Lighthouse");
-	} else if (num % 3 === 0 && num % 4 === 0) {
-	  console.log("LoopyLighthouse");
+	if (i % 3 === 0 && i % 4 === 0) {
+		// filters the numbers that are multiples of 3 and 4 first
+		console.log("LoopyLighthouse");
+	} else if (i % 3 === 0) {
+		// filters numbers that are multiples of only 3
+		console.log("Loopy");
+	} else if (i % 4 === 0) {
+		// filters numbers that are multiples of 4 only
+		console.log("Lighthouse");
 	} else {
+		// and finally, logs all other numbers
 		console.log(i);
 	}
 }
